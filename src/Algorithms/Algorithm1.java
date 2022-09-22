31cc1777054cd1be4f373ffb2d2c7358e55a1d40
Yomi <mustaphaabayomi71@gmail.com>
2022-09-23 00:05:20 +0100

    Alogrithms

---
+++ src/Algorithms/Algorithm1.java
@@ -0,0 +1,22 @@
+package Algorithms;
+
+import java.util.List;
+
+public class Algorithm1 {
+    public static void main(String[] args) {
+
+        //Given an array of integers, find the sum of its elements.
+        // For example, if the array ar = [1, 2, 3], 1 +2 + 3 = 6 , so return 6.
+
+        int[] arr = {1, 2, 3, 5, 6, 2, 6, 2};
+
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        System.out.println(sum);
+
+    }
+}
