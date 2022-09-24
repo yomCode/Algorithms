bb5df07fa28c281d94bf822a8ea426f7c885b6b3
Yomi <mustaphaabayomi71@gmail.com>
2022-09-24 01:04:39 +0100

    Algorithms

---
+++ src/Algorithms/Algorithm3.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+import java.util.List;
+
+public class Algorithm3 {
+    public static long aVeryBigSum(List<Long> ar) {
+        //You are required to calculate and print the sum of the elements in an array,
+        // keeping in mind that some of those integers may be quite large.
+
+        Long result = 0L;
+
+        for(Long num : ar){
+            result += num;
+        }
+        return result;
+    }
+
+}
